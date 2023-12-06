8bb3fdee69974471e9850d8e9dfee403d2270a9c
Sponchiatto <rafael_sponchiatto@hotmail.com>
2023-12-06 16:53:01 -0300

    crud-aplicação-fokus-completa

--- script-crud.js
+++ script-crud.js
@@ -2,13 +2,16 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task"); //Botão Adicionar tarefa
 const formAdicionarTarefa = document.querySelector(".app__form-add-task"); //Formulário de Adicionar Tarefa
 const textArea = document.querySelector(".app__form-textarea"); //Area dee texto do formulário
-const ulTarefas = document.querySelector(".app__section-task-list");
+const ulTarefas = document.querySelector(".app__section-task-list"); // Selecionando formulário de adicionar tarefa
 const btnCancelar = document.querySelector(".app__form-footer__button--cancel"); // Selecione o botão de Cancelar que adicionamos ao formulário
 const paragrafoDescricaoTarefa = document.querySelector(
   ".app__section-active-task-description"
-);
+); //Selecionando o elemento que vai mostrar a tarefa selecionada
 
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []; //aqui precisamos fazer o inverso do que foi feito no stringfy
+const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas"); //Botão de remover as tarefas concluídas
+const btnRemoverTodas = document.querySelector("#btn-remover-todas"); //Botão de remover todas as tarefas
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []; //aqui precisamos fazer o inverso do que foi feito no stringfy
 let tarefaSelecionada = ""; // objeto da tarefa selecionada
 let liTarefaSelecionada = ""; // elemento html  item de lista da tarefa selecionada
 
@@ -67,28 +70,33 @@ function criarElementoTarefa(tarefa) {
   li.append(paragrafo);
   li.append(botao);
 
-  //Método para Evidênciar as tarefas ao clicar e deselecionar
-  li.onclick = () => {
-    document
-      .querySelectorAll(".app__section-task-list-item-active")
-      .forEach((elemento) => {
-        elemento.classList.remove("app__section-task-list-item-active");
-      });
+  if (tarefa.completa) {
+    li.classList.add("app__section-task-list-item-complete");
+    botao.setAttribute("disabled", "disabled");
+  } else {
+    //Método para Evidênciar as tarefas ao clicar e deselecionar
+    li.onclick = () => {
+      document
+        .querySelectorAll(".app__section-task-list-item-active")
+        .forEach((elemento) => {
+          elemento.classList.remove("app__section-task-list-item-active");
+        });
 
-    // Deselecionar a tarefa que está selecionada
-    if (tarefaSelecionada == tarefa) {
-      paragrafoDescricaoTarefa.textContent = "";
-      tarefaSelecionada = null;
-      liTarefaSelecionada = null;
-      return; // Early Return, para terminar de executar o código por aqui
-    }
+      // Deselecionar a tarefa que está selecionada
+      if (tarefaSelecionada == tarefa) {
+        paragrafoDescricaoTarefa.textContent = "";
+        tarefaSelecionada = null;
+        liTarefaSelecionada = null;
+        return; // Early Return, para terminar de executar o código por aqui
+      }
 
-    tarefaSelecionada = tarefa;
-    liTarefaSelecionada = li;
-    paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+      tarefaSelecionada = tarefa;
+      liTarefaSelecionada = li;
+      paragrafoDescricaoTarefa.textContent = tarefa.descricao;
 
-    li.classList.add("app__section-task-list-item-active");
-  };
+      li.classList.add("app__section-task-list-item-active");
+    };
+  }
 
   return li;
 }
@@ -120,9 +128,12 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
   formAdicionarTarefa.classList.add("hidden");
 });
 
+// Para cada tarefa no array de tarefas
 tarefas.forEach((tarefa) => {
+  // Cria um elemento de tarefa usando a função criarElementoTarefa, passando a tarefa como argumento
   const elementoTarefa = criarElementoTarefa(tarefa);
 
+  // Adiciona o elemento de tarefa criado à lista ulTarefas (provavelmente uma <ul> na sua estrutura HTML)
   ulTarefas.append(elementoTarefa);
 });
 
@@ -135,12 +146,50 @@ const limparFormulario = () => {
 // Associe a função limparFormulario ao evento de clique do botão Cancelar
 btnCancelar.addEventListener("click", limparFormulario);
 
+
+// Adiciona um event listener para o evento "FocoFinalizado, cuja referência encontra-se em script.js"
 document.addEventListener("FocoFinalizado", () => {
+  // Verifica se tarefaSelecionada e liTarefaSelecionada estão definidos e são verdadeiros
   if (tarefaSelecionada && liTarefaSelecionada) {
+    // Remove a classe "app__section-task-list-item-active" do elemento liTarefaSelecionada
     liTarefaSelecionada.classList.remove("app__section-task-list-item-active");
+    
+    // Adiciona a classe "app__section-task-list-item-complete" ao elemento liTarefaSelecionada
     liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
+    
+    // Desabilita o botão dentro do elemento liTarefaSelecionada
     liTarefaSelecionada
       .querySelector("button")
       .setAttribute("disabled", "disabled");
+    
+    // Define a propriedade completa da tarefaSelecionada como true
+    tarefaSelecionada.completa = true;
+    
+    // Chama a função atualizarTarefas() após as operações serem realizadas
+    atualizarTarefas();
   }
 });
+
+// Definição da função removerTarefas que recebe um parâmetro booleano somenteCompletas
+const removerTarefas = (somenteCompletas) => {
+  // Define o seletor com base no parâmetro somenteCompletas
+  const seletor = somenteCompletas
+    ? ".app__section-task-list-item-complete" // Se somenteCompletas for verdadeiro, seleciona elementos completos
+    : ".app__section-task-list-item"; // Caso contrário, seleciona todos os elementos
+  
+  // Remove todos os elementos encontrados pelo seletor
+  document.querySelectorAll(seletor).forEach((elemento) => {
+    elemento.remove();
+  });
+
+  // Atualiza a variável tarefas com base no parâmetro somenteCompletas
+  tarefas = somenteCompletas
+    ? tarefas.filter((tarefa) => !tarefa.completa) // Se somenteCompletas for verdadeiro, filtra as tarefas não completas
+    : []; // Caso contrário, define a lista de tarefas como vazia
+  
+  // Chama a função atualizarTarefas após a remoção/alteração das tarefas
+  atualizarTarefas();
+};
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true);
+btnRemoverTodas.onclick = () => removerTarefas(false);
